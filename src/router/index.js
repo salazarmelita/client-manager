55df0b6572aa367ec10e567031ca4fa9ee92346a
salazarmelita <asalazar@hyl.cl>
2023-07-24 10:42:28 -0400

    feat: initial configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Inicio from '../views/ClientesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // arr5eglo para multiples páginas
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: Inicio
     }
   ]
 })
---
+++ src/views/ClientesView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Clientes</h1>
+    </div>
+</template>
